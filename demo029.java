3d8c669ccb08c800bb25eab9a0f1b241fe15300b
velidivasu27 <vasuvelidi27@gmail.com>
2025-01-26 20:11:37 +0530

    Add files via upload

---
+++ demo029.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class demo029 {
+    public static void main(String[] args) {
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("manu");
+        friends.add("Roshi");
+        friends.add("himu");
+        friends.add("Navya");
+        friends.add("Nagashree");
+        friends.add("Deepuu");
+        friends.add("shamii");
+        friends.add("Sathwika");
+        friends.add("tanu");
+        friends.add("pravallika");
+
+        System.out.println(friends);
+    }
+}
