9f0361a88997ab8f3af8a1ebe89706376ca88833
velidivasu27 <vasuvelidi27@gmail.com>
2025-01-26 20:08:20 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,17 @@
+public class demo2 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+    System.out.print(s:"Enter the first number: ");
+int num1 = scanner.nextInt();
+System.out.print(s:"Enter the second number: ");
+int num2 = scanner.nextInt();
+if (num1 > num2) {
+    System.out.println("The greatest number is:" +num1);
+ } if else(num2 > num1){
+        System.out.println("The greatest number is:" +num2);
+        
+} else {
+    System.out.println(x:"Both numbers are equal.");
+    
+}
+scanner.close();
