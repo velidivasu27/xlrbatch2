5b4935e143a140dbb0284555f803e1d9091081bf
velidivasu27 <vasuvelidi27@gmail.com>
2025-01-26 20:09:20 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String name ="navya";
+        String pass = "navya@2005";
+        while((name == "navya")&&(pass =="navya@2005")){
+        System.out.println("hello navya welcome to malla reddy university");
+        break;
+    }
+    }
+    
+}
