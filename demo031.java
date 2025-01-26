d454f37063fde6dec73f30f1f645d9fa17865d6a
velidivasu27 <vasuvelidi27@gmail.com>
2025-01-26 20:12:43 +0530

    Add files via upload

---
+++ demo031.java
@@ -0,0 +1,24 @@
+
+ import java.util.HashSet;
+class HashSetDemo1 {
+	void HSetMethod() {
+		
+		HashSet hashSet = new HashSet();
+		hashSet.add(100);
+		hashSet.add("navya");
+		hashSet.add(9999.99);
+		System.out.println(hashSet);		
+
+		for(Object x: hashSet) {
+			System.out.println(x);
+		}
+	}
+}
+
+
+public class demo031 {
+	public static void main(String[] args) {
+		HashSetDemo1 obj = new HashSetDemo1();
+		obj.HSetMethod();
+		}
+	}
