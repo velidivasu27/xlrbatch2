6c4638cc6f5de9d7801d6a400ec9a4b578f5d789
velidivasu27 <vasuvelidi27@gmail.com>
2025-01-26 20:06:50 +0530

    Add files via upload

---
+++ DisplayNumbers.java
@@ -0,0 +1,8 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        // Loop to print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
