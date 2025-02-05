96bc4288566a8911eb8c4dc7e1785d4e49448a93
velidivasu27 <vasuvelidi27@gmail.com>
2025-02-05 10:18:06 +0530

    sum first and last array

---
+++ demo063.java
@@ -0,0 +1,11 @@
+public class demo063 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        if (arr.length > 0) {
+            int sum = arr[0] + arr[arr.length - 1];
+            System.out.println("Sum of First and Last Element: " + sum);
+        } else {
+            System.out.println("Array is empty.");
+ }
+}
+} 
